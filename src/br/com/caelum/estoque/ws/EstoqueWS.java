1817fe1cbd8ee425d84d2a67adccb80334989cf1
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-09-29 10:44:31 -0300

    [Fix] Melhorando a indentação de algumas classes

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -7,13 +7,13 @@ import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import br.com.caelum.estoque.exception.AutorizacaoException;
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ItemValidador;
 import br.com.caelum.estoque.modelo.item.ListaItens;
-import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
 import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
@@ -22,30 +22,31 @@ public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
 
-    @WebMethod(operationName="todosOsItens") 
-    @WebResult(name="itens")
+    @WebMethod(operationName = "todosOsItens") 
+    @WebResult(name = "itens")
     public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
-        System.out.println("Chamando getItens()");
+        System.out.println("Chamando todosItens()");
         List<Filtro> lista = filtros.getLista();
         List<Item> itensResultado = dao.todosItens(lista);
         return new ListaItens(itensResultado);
     }
 	
-    @WebMethod(operationName="CadastrarItem") 
+    @WebMethod(operationName = "CadastrarItem") 
     public Item cadastrarItem(
-    		@WebParam(name="tokenUsuario", header=true) TokenUsuario token, 
-    		@WebParam(name="item") Item item) throws AutorizacaoException {
+    		@WebParam(name = "tokenUsuario", header = true) TokenUsuario token, 
+    		@WebParam(name = "item") Item item) throws AutorizacaoException {
 
-        System.out.println("Cadastrando " + item + ", " + token);
-
-        if(! new TokenDao().ehValido(token)) {
+        if(!new TokenDao().ehValido(token)) {
             throw new AutorizacaoException("Autorizacao falhou");
         }
 
+        System.out.println("Cadastrando " + item + ", " + token);
+        
         //novo
         new ItemValidador(item).validate();
 
         this.dao.cadastrar(item);
+        
         return item;
     }
 }
