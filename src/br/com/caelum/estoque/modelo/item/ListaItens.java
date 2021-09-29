2c3b18df27cf4d328c9b7ecc8d1384f4b0192d8e
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-09-29 10:43:39 -0300

    [Fix] Melhorando a indentação de algumas classes

--- src/br/com/caelum/estoque/modelo/item/ListaItens.java
+++ src/br/com/caelum/estoque/modelo/item/ListaItens.java
@@ -10,6 +10,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ListaItens {
+	
 	@XmlElement(name="item")
 	private List<Item> itens;
 
