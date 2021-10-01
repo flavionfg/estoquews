141aa25c847c1d2b946258d8eda99e1d497ea6f3
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-10-01 10:22:39 -0300

    Gerando o WSDL

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -9,6 +9,7 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.ParameterStyle;
 import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
 import br.com.caelum.estoque.exception.AutorizacaoException;
 import br.com.caelum.estoque.modelo.item.Filtro;
@@ -16,12 +17,13 @@ import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ItemValidador;
+import br.com.caelum.estoque.modelo.item.ItemValidadorException;
 import br.com.caelum.estoque.modelo.item.ListaItens;
 import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
-@SOAPBinding(style=Style.DOCUMENT,parameterStyle=ParameterStyle.BARE)
+@SOAPBinding(style=Style.DOCUMENT, use=Use.LITERAL,parameterStyle=ParameterStyle.WRAPPED)
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
@@ -35,11 +37,11 @@ public class EstoqueWS {
         return new ListaItens(itensResultado);
     }
 	
-    @WebMethod(operationName = "CadastrarItem") 
-    public Item cadastrarItem(
-    		@WebParam(name = "tokenUsuario", header = true) TokenUsuario token, 
-    		@WebParam(name = "item") Item item) throws AutorizacaoException {
-
+    @WebMethod(action="CadastrarItem", operationName="CadastrarItem") 
+    @WebResult(name="item")
+    public Item cadstrarItem(
+    		@WebParam(name="tokenUsuario", header=true) TokenUsuario token,
+    		@WebParam(name="item") Item item) throws ItemValidadorException, AutorizacaoException {
         if(!new TokenDao().ehValido(token)) {
             throw new AutorizacaoException("Autorizacao falhou");
         }
